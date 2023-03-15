4f037a98afb5d227d2d4f506d24a58bb2ef8b4ec
Yuliya Maslova <uu.naumova@gmail.com>
2023-03-15 09:39:50 +0300

    change in method devide

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -27,17 +27,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> {
-        if (y != 0) {
-            return x / y;
-        }
-        if (y == 0) {
-            System.out.println("На ноль делить нельзя!");
-        } else {
-            throw new ArithmeticException("Ошибка! На 0 делить нельзя.");
-        }
-        return x;
-    };
+    BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : Integer.MAX_VALUE;
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
